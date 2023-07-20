b18e17aac8de89b17035b7a92588182647e5509f
Paul Kallarackel <paulkallarackel@gmail.com>
2023-07-20 17:26:52 +0100

    working play and reset buttons with logic

--- game.js
+++ game.js
@@ -1,8 +1,7 @@
 const Model = (() => {
 	const gameBoard = (() => {
 		let availSpot;
-
-		const init = () => {
+		const init = function () {
 			availSpot = [];
 			for (let i = 1; i < 4; i++) {
 				for (let j = 1; j < 4; j++) {
@@ -41,10 +40,12 @@ const Model = (() => {
 
 	let player1;
 	let player2;
+	let gameWon;
 
 	const init = function () {
 		// Change to a regular function expression
 		gameBoard.init();
+		gameWon = false;
 		this.player1 = playerFactory(); // 'this' will refer to the 'Model' object
 		this.player2 = playerFactory(); // 'this' will refer to the 'Model' object
 		// You can now use player1 and player2 as needed within the Model module.
@@ -55,14 +56,37 @@ const Model = (() => {
 })();
 
 const View = (() => {
-	const init = () => {
+	const playButton = document.getElementById("playButton");
+	const resetButton = document.getElementById("resetButton");
+	const marker = document.getElementById("marker");
+
+	const init = function () {
 		// Select the SVG elements with the class "svg" and hide them on page load
 		const svgElements = document.querySelectorAll(".gridMarker");
 		for (const marker of svgElements) {
-			marker.classList.add("d-none");
+			marker.querySelector("svg").classList.add("d-none");
+			marker.addEventListener("click", Controller.handleClick);
 		}
+		this.playButton.addEventListener("click", Controller.handlePlay);
+		this.resetButton.addEventListener("click", Controller.handleReset);
 	};
-	return { init };
+	const setMarker = function (clickedIcon, marker) {
+		let svg;
+		switch (marker) {
+			case "X":
+				svg = "cross-46.svg#cross";
+				break;
+			case "O":
+				svg = "circle-ring.svg#ring";
+				break;
+            default :
+                return
+                
+		}
+		clickedIcon.querySelector("use").setAttribute("xlink:href", svg);
+		clickedIcon.classList.remove("d-none");
+	};
+	return { init, setMarker, playButton, resetButton, marker };
 })();
 
 const Controller = (() => {
@@ -70,7 +94,30 @@ const Controller = (() => {
 		View.init();
 		Model.init();
 	};
-	return { init };
+	const handlePosition = function (clickedPosition) {
+		if (Model.gameWon) return false;
+		return true;
+	};
+	const handleClick = function (e) {
+		let clickedIcon = e.target;
+		let clickedPosition = e.target.querySelector("svg").id;
+		if (Controller.handlePosition(clickedPosition)) {
+			View.setMarker(clickedIcon.querySelector("svg"), Model.player1.marker);
+		}
+		Model.gameWon ? alert("You Won!") : undefined;
+	};
+	const handlePlay = function (e) {
+		Model.player1.marker = View.marker.options[View.marker.selectedIndex].value;
+		Model.player1.marker == "X"
+			? (Model.player2.marker = "O")
+			: (Model.player2.marker = "X");
+		console.log("Player 1: ", Model.player1.marker);
+		console.log("Player 2: ", Model.player2.marker);
+	};
+	const handleReset = function (e) {
+		Controller.init()
+	};
+	return { init, handlePosition, handleClick, handleReset,handlePlay };
 })();
 
 export { Model, View, Controller };
--- script.js
+++ script.js
@@ -1,5 +1,2 @@
 import { Model, View, Controller } from "./game.js";
 Controller.init(); // Initialize the Model and get the returned objects.
-console.log(Model.gameBoard.getAvailSpot()); 
-Model.player1.play('r1c1'); 
-console.log(Model.gameBoard.getAvailSpot()); 
